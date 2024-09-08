442204fc5f8314210d8262ff0824d90b4cdd4a6b
Rikikesh <adarshamishra98@gmail.com>
2024-09-08 19:52:23 +0545

    removed title for the POST class

--- SocialWebsite/post/models.py
+++ SocialWebsite/post/models.py
@@ -2,10 +2,9 @@ from django.db import models
 from userdetail.models import Profile
 from django.conf import settings
 from django.utils.text import slugify
-
+import uuid
 class Post(models.Model):
     user=models.ForeignKey(Profile,on_delete=models.CASCADE)
-    title=models.CharField(max_length=50)
     slug = models.SlugField(null=False, unique=True)
     body=models.CharField(max_length=280,blank=True)
     image=models.ImageField(blank=True,upload_to='static/post_images')
@@ -13,11 +12,25 @@ class Post(models.Model):
     created=models.DateTimeField(auto_now_add=True)
     likes=models.ManyToManyField(Profile,related_name="post_like",blank=True)
 
+    def generate_slug(self):
+        if self.body:
+            slug = slugify( +"-" +str(self.id))
+        else:
+            slug = slugify(f"post-{uuid.uuid4().hex[:8]}")
+        unique_slug = slug
+        num = 1
+        while Post.objects.filter(slug=unique_slug).exists():
+            unique_slug = f"{slug}-{num}"
+            num += 1
+        return unique_slug
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug=slugify(self.title)
+            self.slug = self.generate_slug()
         super(Post,self).save(*args,**kwargs)
+   
 
+    def count_like(self):
+        return self.likes.count()
 
     def __str__(self):
         return f"{self.user} post created on  {self.created} ."
