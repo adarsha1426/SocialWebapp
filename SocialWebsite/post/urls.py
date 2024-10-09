80de11d36da2116ee28177fd22369b0604a08c16
Rikikesh <adarshamishra98@gmail.com>
2024-10-09 22:39:03 +0545

    shifted from post id to post slug

--- SocialWebsite/post/urls.py
+++ SocialWebsite/post/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path('like/<slug:post_slug>',views.like,name="like"),
     path('comment/<slug:post_slug>',views.create_comment,name="create_comment"),
     path('profile/<str:username>',views.postdetail,name="your_profile"),
-    path('user_post/<int:id>',views.your_post,name="your_post"),
-    path('delete_post/<int:post_id>',views.delete_post,name="delete_post"),
-    path('delete/<int:post_id>',views.delete,name="delete"),
+    path('user_post/<slug:post_slug>',views.your_post,name="your_post"),
+    path('delete_post/<slug:post_slug>',views.delete_post,name="delete_post"),
+    path('delete/<slug:post_slug>',views.delete,name="delete"),
 
     path('',include('userdetail.urls'),name="userdetail"),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
